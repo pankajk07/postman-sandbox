68ec4c6214bee20363bf0ca473bd05155cb5b568
Shamasis Bhattacharya <mail@shamasis.net>
2020-06-13 19:50:23 +0530

    Fixed test specs that were missing describe blocks. (Note that browser tests are not currently all working)

--- test/unit/sandbox-timeout.test.js
+++ test/unit/sandbox-timeout.test.js
@@ -1,4 +1,6 @@
-('sandbox timeout', function () {
+// @todo find ways to support basic timeout, if not the real ones that protect against infinite loops, but at least
+// ones that can emulate the timeout APIs
+(typeof window === 'undefined' ? describe : describe.skip)('sandbox timeout', function () {
     this.timeout(1000 * 60);
     var Sandbox = require('../../lib');
 
--- test/unit/sandbox-timers.test.js
+++ test/unit/sandbox-timers.test.js
@@ -1,4 +1,4 @@
-('timers inside sandbox', function () {
+describe('timers inside sandbox', function () {
     this.timeout(1000 * 60);
     var Sandbox = require('../../lib'),
         ctx;
